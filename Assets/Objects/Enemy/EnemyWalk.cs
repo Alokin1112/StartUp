fac438906dc007aaed97386bb8f7a454e76992e1
Alokin1112 <davidson1@o2.pl>
2021-03-13 12:59:55 +0100

    Added architecture folder, added player and enemy rotating,modified player movement

--- Assets/Objects/Enemy/EnemyWalk.cs
+++ Assets/Objects/Enemy/EnemyWalk.cs
@@ -9,10 +9,13 @@ public class EnemyWalk : MonoBehaviour
     public float enemyMovementSpeed = .1f;
     public float minDistance = 1f;
     public float maxDistance = 5f;
+    private float direction;
+    private Vector2 Rotation;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Rotation = new Vector2(transform.localScale.x, transform.localScale.y);
     }
 
     // Update is called once per frame
@@ -22,15 +25,29 @@ public class EnemyWalk : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, enemyMovementSpeed * Time.deltaTime);
         }
+        direction = player.transform.position.x - transform.position.x;
+        RotateEnemy(direction);
     }
     public void Stun(float amountOfTime)
     {
         changeStun();
         Debug.Log("Enemy Stunned");
+        enemyMovementSpeed = enemyMovementSpeed * 0.75f;
         Invoke("changeStun", amountOfTime);
     }
     private void changeStun()
     {
         isNotStunned = !isNotStunned;
+    }
+    private void RotateEnemy(float direction)
+    {
+        if (direction > 0f)
+        {
+            transform.localScale = Rotation;
+        }
+        else if (direction < 0f)
+        {
+            transform.localScale = new Vector2(-Rotation.x, Rotation.y);
+        }
     }
 }
--- Assets/Objects/Player/PlayerMove.cs
+++ Assets/Objects/Player/PlayerMove.cs
@@ -7,10 +7,14 @@ public class PlayerMove : MonoBehaviour
 
     public float playerSpeed = 4f;
     public GameManager gameManager;
+    private Vector2 Rotation;
+    private Vector2 Movement;
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-
+        Rotation = new Vector2(transform.localScale.x, transform.localScale.y);
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +23,31 @@ public class PlayerMove : MonoBehaviour
         if (gameManager.canMove)
         {
             float moveX = Input.GetAxis("Horizontal");
-            transform.Translate(playerSpeed * moveX * Time.deltaTime, 0, 0);
+            RotatePlayer(moveX);
+            Movement = new Vector2(moveX, 0);
+        }
+        else
+        {
+            Movement = new Vector2(0, 0);
+        }
+    }
+    private void FixedUpdate()
+    {
+        moveCharacter(Movement);
+    }
+    private void moveCharacter(Vector2 direction)
+    {
+        rigidbody.velocity = direction * playerSpeed;
+    }
+    private void RotatePlayer(float direction)
+    {
+        if (direction > 0f)
+        {
+            transform.localScale = Rotation;
+        }
+        else if (direction < 0f)
+        {
+            transform.localScale = new Vector2(-Rotation.x, Rotation.y);
         }
     }
 }
