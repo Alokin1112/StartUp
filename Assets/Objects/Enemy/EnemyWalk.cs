12faa868f26ea154311084bcda0e632e6fb766af
Alokin1112 <davidson1@o2.pl>
2021-03-25 18:55:08 +0100

    Added Basic AI to Enemies

--- Assets/Objects/Enemy/EnemyWalk.cs
+++ Assets/Objects/Enemy/EnemyWalk.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyWalk : MonoBehaviour
 {
     private bool isNotStunned = true;
+    private bool isNotChasing = true;
+    private float walkingTime = 2.5f;
     private Transform player;
     public float enemyMovementSpeed = .1f;
     public float minDistance = 1f;
@@ -12,6 +14,7 @@ public class EnemyWalk : MonoBehaviour
     private float direction;
     private Vector2 Rotation;
     private Vector2 dir;
+    private Vector2 normalWalkingSpeed = new Vector2(2, 0);
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,7 @@ public class EnemyWalk : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         Rotation = new Vector2(transform.localScale.x, transform.localScale.y);
         rb = GetComponent<Rigidbody2D>();
+        StartWalking();
     }
 
     // Update is called once per frame
@@ -26,16 +30,22 @@ public class EnemyWalk : MonoBehaviour
     {
         if (isNotStunned && Vector2.Distance(transform.position, player.position) > minDistance && Vector2.Distance(transform.position, player.position) < maxDistance)
         {
+            isNotChasing = false;
             //transform.position = Vector2.MoveTowards(transform.position, player.position, enemyMovementSpeed * Time.deltaTime);
             direction = player.transform.position.x - transform.position.x;
             RotateEnemy(direction);
             dir = new Vector2(direction % 1, 0);
             moveEnemy(dir);
         }
-        else
+        else if (!isNotStunned)
         {
             dir = new Vector2(0, 0);
             moveEnemy(dir);
+        }
+        else
+        {
+            isNotChasing = true;
+
         }
     }
     public void Stun(float amountOfTime)
@@ -59,6 +69,25 @@ public class EnemyWalk : MonoBehaviour
         {
             transform.localScale = new Vector2(-Rotation.x, Rotation.y);
         }
+    }
+    private void StopWalking()
+    {
+        Vector2 velocity = rb.velocity;
+        Debug.Log("Zatrzymuje sie");
+        if (isNotChasing)
+            rb.velocity = new Vector2(0, 0);
+        Invoke("StartWalking", walkingTime);
+    }
+    private void StartWalking()
+    {
+        normalWalkingSpeed *= (-1f);
+        Debug.Log("Chodze");
+        if (isNotChasing)
+        {
+            rb.velocity = normalWalkingSpeed;
+            RotateEnemy(normalWalkingSpeed.x);
+        }
+        Invoke("StopWalking", walkingTime);
     }
     private void moveEnemy(Vector2 dir)
     {
